140bdda2aeb6b85b32c5d93fd540bd8cc877f8cb
jamesjohnstone <jamesjohnstone15@gmail.com>
2018-06-24 17:15:09 +0100

    Sort module declaration order

--- src/Client.hs
+++ src/Client.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Client (
-    quota
+    convert
+    , getQuote
+    , getQuotes
     , getSymbols
     , marketIsOpen
-    , convert
-    , getQuotes
+    , quota
     ) where
 
 import Data.List
@@ -16,22 +17,32 @@ import Network.HTTP.Simple
 import JSONTypes
 
 baseURL = "https://forex.1forge.com/1.0.3/"
-apiKey = -- "YOUR API KEY HERE"
+apiKey = "YOUR API KEY HERE" -- obtain an API key from https://1forge.com/forex-data-api
 
 quota :: IO (Maybe Quota)
 quota = do
     response <- fetch "quota?cache=false"
-    return (getResponseBody response :: Maybe Quota)
+    return (getResponseBody response :: Maybfetch Quota)
 
 getSymbols :: IO (Maybe [String])
 getSymbols = do
     response <- fetch "symbols?cache=false"
     return (getResponseBody response :: Maybe [String])
 
-getQuotes :: [String] -> IO (Maybe [Quotes])
+getQuotes :: [String] -> IO (Maybe [Quote])
 getQuotes pairs = do 
     response <- fetch $ "quotes?pairs=" ++ intercalate "," pairs
-    return (getResponseBody response :: Maybe [Quotes])
+    return (getResponseBody response :: Maybe [Quote])
+
+getQuote :: String -> IO (Maybe Quote)
+getQuote pairs = do 
+    response <- fetch $ "quotes?pairs=" ++ pairs
+    return (getSingleQuote (getResponseBody response :: Maybe [Quote]))
+
+getSingleQuote :: Maybe [Quote] -> Maybe Quote
+getSingleQuote quotes = do
+    q <- quotes
+    return (head q)
 
 marketIsOpen :: IO (Maybe MarketStatus)
 marketIsOpen = do
